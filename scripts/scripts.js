5aedf076e13b157b0d69c3daae96b2a6c29792b6
heman20071109-maker <Heman20071109@gmail.com>
2026-05-13 09:30:28 +0800

    3changes
    Signed-off-by: heman20071109-maker <Heman20071109@gmail.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,15 @@
-// 功能 1：按钮点击改变网页状态
-const myButton = document.getElementById("magicButton");
-const myTitle = document.getElementById("main-title");
 
-myButton.addEventListener("click", function() {
-    document.body.style.backgroundColor = "#dff9fb";
-    myTitle.innerText = "Thanks for visiting my site! ✨";
-    myButton.innerText = "Glad you are here!";
-    myButton.style.backgroundColor = "#27ae60";
+const btn = document.getElementById("magicButton");
+btn.addEventListener("click", () => {
+    alert("Hello! I'm Herman. Thanks for checking out my work!");
+    btn.style.backgroundColor = "#00cec9";
+    btn.innerText = "Welcome!";
 });
 
-// 功能 2：为所有图片添加点击弹窗效果 (Unit 9 互动进阶)
-const allImages = document.querySelectorAll('.image-card img');
 
-// 使用循环为每一张图片绑定点击事件
-allImages.forEach(function(img) {
-    img.addEventListener("click", function() {
-        // img.alt 会读取你在 HTML 中写在 alt="..." 里的文字
-        alert("You clicked on: " + img.alt + "! It's a great picture.");
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        const title = card.querySelector('h3').innerText;
+        console.log(`User is interested in: ${title}`);
     });
 });
